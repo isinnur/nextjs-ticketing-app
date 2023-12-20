6f002b9464af45ea761887531c44b9082d7f882d
isinnur <isinngnyy@gmail.com>
2023-12-20 22:21:17 +0300

    feat: connect mongodb

--- app/(components)/TicketCard.jsx
+++ app/(components)/TicketCard.jsx
@@ -17,8 +17,16 @@ const TicketCard = () => {
       <p className="whitespace-pre-wrap">
         this is the ticket description. please do this ticket
       </p>
-      <ProgressDisplay />
-      <StatusDisplay />
+      <div className="flex-grow"></div>
+      <div className="flex mt-2">
+        <div className="flex flex-col">
+          <p className="text-xs my-1">08/31/23 10:43PM</p>
+          <ProgressDisplay />
+        </div>
+        <div className="ml-auto flex items-end">
+          <StatusDisplay />
+        </div>
+      </div>
     </div>
   );
 };
