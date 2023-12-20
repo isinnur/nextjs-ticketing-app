a60589a1a8c1935e4d5138b821c53bd16d2bbdb0
isinnur <isinngnyy@gmail.com>
2023-12-20 23:05:48 +0300

    feat: add ticket form

---
+++ app/api/route.js
@@ -0,0 +1,12 @@
+import Ticket from "../(models)/Ticket";
+import { NextResponse } from "next/server";
+export async function POST(reg) {
+  try {
+    const body = await req.json();
+    const ticketData = body.formData;
+    await Ticket.create(ticketData);
+    return NextResponse.json({ message: "Ticket Created" }, { status: 201 });
+  } catch (error) {
+    return NextResponse.json({ message: "Error", error }, { status: 500 });
+  }
+}
