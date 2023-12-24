0a3626c206b333c697994d25ba6bc4a7fef64544
isinnur <isinngnyy@gmail.com>
2023-12-24 19:16:56 +0300

    feat: add format date

--- app/(components)/TicketCard.jsx
+++ app/(components)/TicketCard.jsx
@@ -4,6 +4,21 @@ import ProgressDisplay from "./ProgressDisplay";
 import StatusDisplay from "./StatusDisplay";
 
 const TicketCard = ({ ticket }) => {
+  const formatTimestamp = (timestamp) => {
+    const options = {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+      hour: "2-digit",
+      minutea: "2-digit",
+      hour12: true,
+    };
+    const date = new Date(timestamp);
+    const formattedDate = date.toLocaleDateString("en-us", options);
+
+    return formattedDate;
+  };
+
   return (
     <div className="flex flex-col bg-card hover:bg-card-hover rounded-md shadow-lg p-3 m-2">
       <div className="flex mb-3 ">
@@ -18,7 +33,7 @@ const TicketCard = ({ ticket }) => {
       <div className="flex-grow"></div>
       <div className="flex mt-2">
         <div className="flex flex-col">
-          <p className="text-xs my-1">{ticket.createdAt}</p>
+          <p className="text-xs my-1">{formatTimestamp(ticket.createdAt)}</p>
           <ProgressDisplay progress={ticket.progress} />
         </div>
         <div className="ml-auto flex items-end">
