761509352a0d1955f2dfc16050b197f7208f9488
isinnur <isinngnyy@gmail.com>
2023-12-25 12:53:50 +0300

    feat: add edit mode

--- app/TicketPage/[id]/page.jsx
+++ app/TicketPage/[id]/page.jsx
@@ -1,6 +1,5 @@
 import TicketForm from "@/app/(components)/TicketForm";
 
-
 const getTicketById = async (id) => {
   try {
     const res = await fetch(`http:localhost:3000/api/Tickets/${id}`, {
@@ -10,18 +9,24 @@ const getTicketById = async (id) => {
     if (!res.ok) {
       throw new Error("Failed to get ticket.");
     }
+    return res.json();
+  } catch (error) {
+    console.log(error);
   }
- return res.json();
-}
+};
 
 const TicketPage = async ({ params }) => {
   const EDITMODE = params.id === "new" ? false : true;
   let updateTicketData = {};
   if (EDITMODE) {
     updateTicketData = await getTicketById(params.id);
-    console.log(updateTicketData);
+    updateTicketData = updateTicketData.foundTicket;
+  } else {
+    updateTicketData = {
+      _id: "new",
+    };
   }
-  return <TicketForm />;
+  return <TicketForm ticket={updateTicketData} />;
 };
 
 export default TicketPage;
